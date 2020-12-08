269391503e97a49053611b1e24c20f1d169d1d06
jamin12 <rudals9901@naver.com>
2020-12-08 23:52:25 +0900

    .

--- python/RPA/1_excel/3_cell.py
+++ python/RPA/1_excel/3_cell.py
@@ -19,9 +19,21 @@ print(ws["A10"].value)  # 값이 없을 땐 None을 출력
 
 # row = 1, 2, 3
 # column = A B C
-ws.cell(row=1, column=1)
-print(ws.cell(row=1, column=1).value)
+print(ws.cell(row=1, column=1).value) # ws["A1"].value
+print(ws.cell(row=1, column=2).value) # ws["B1"].value
 
-ws.cell(column=3, row=1, value=10)
+c = ws.cell(column=3, row=1, value=10) # ws["c1"].value = 10
+print(c.value) # ws['C1']
+
+from random import *
+
+index = 1
+# 반복문을 이용해서 랜덤 숫자 채우기
+for x in range(1,11): # 10 개 row
+    for y in range(1,11): # 10 개 column
+        # ws.cell(row = x, column=y, value=randint(0, 100)) # 0~100 사이의 숫자 
+        ws.cell(row=x, column=y, value=index)
+        index += 1
+
 
 wb.save("sample.xlsx")
