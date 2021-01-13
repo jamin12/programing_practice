321e9224cd7105bee8e7386d1cf6fb9a62abce7a
jamin12 <rudals9901@naver.com>
2021-01-13 23:50:43 +0900

    장고 학습 끝 복습하쟝

--- python/django_practice/polls/admin.py
+++ python/django_practice/polls/admin.py
@@ -2,5 +2,18 @@ from django.contrib import admin
 from .models import Question,Choice
 # Register your models here.
 
-admin.site.register(Question)
-admin.site.register(Choice)
+
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('test',{'fields':['question_text']}),
+        ('Date information',{'fields':['pub_data']})
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text','pub_data')
+admin.site.register(Question,QuestionAdmin)
+
