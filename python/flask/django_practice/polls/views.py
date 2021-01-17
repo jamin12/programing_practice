b72d587bf36c7da62fdc9af1c0f9095a3e07e0a4
jamin12 <rudals9901@naver.com>
2021-01-17 16:44:38 +0900

    .

--- python/flask/django_practice/polls/views.py
+++ python/flask/django_practice/polls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render,get_object_or_404  
 from django.http import HttpResponse,Http404,HttpResponseRedirect
 from .models import Question,Choice
 from django.template import loader
