7d52898ca8063d48dbf06b97aff6bc31fe29e349
jamin12 <rudals9901@naver.com>
2021-02-11 14:16:39 +0900

    fastapi

---
+++ python/test.py
@@ -0,0 +1,8 @@
+def hihi(*h):
+    for i in h:
+        print(i)
+
+
+a = [1,2,3,45,5]
+
+hihi(*a)
---
+++ python/web_framwork/app/routes/index.py
@@ -0,0 +1,31 @@
+from datetime import datetime
+
+from fastapi import APIRouter,Depends
+from starlette.responses import Response
+from starlette.requests import Request
+from inspect import currentframe as frame
+router = APIRouter()
+
+@router.get('/')
+async def index():
+    """
+    ELB 상태 체크용 API
+    :return:
+    """
+    current_time = datetime.utcnow()
+    return Response(f"Notification API(UTC: {current_time.strftime('%Y/%m/%d %H:%M:%S')})")
+
+@router.get("/test")
+async def index(request: Request):
+    """
+    ELB 상태 체크용 API
+    :return:
+    """
+    print(request.state.user)
+    try:
+        a = 1/0
+    except Exception as e:
+        request.state.inspect = frame()
+        raise e
+    current_time = datetime.utcnow()
+    return Response(f"Notification API (UTC: {current_time.strftime('%Y.%m.%d %H:%M:%S')})")
