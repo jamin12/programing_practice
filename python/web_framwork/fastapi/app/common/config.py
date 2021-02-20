5312a338fa9995afcf067b751b855318e314043f
jamin12 <rudals9901@naver.com>
2021-02-20 17:19:23 +0900

    .

---
+++ python/web_framwork/fastapi/app/common/config.py
@@ -0,0 +1,38 @@
+from dataclasses import dataclass
+from os import path,environ
+
+base_dir = path.dirname(path.dirname(path.dirname(path.abspath(__file__))))
+
+
+@dataclass
+class Config:
+    """
+    기본 Configuration
+    """
+    BASE_DIR = base_dir
+
+    DB_POOL_RECYCLE: int = 900
+    DB_ECHO: bool = False
+
+#개발 모드
+@dataclass
+class LocalConfig(Config):
+    DB_URL: str = "mysql+pymysql://root:1234@localhost:3306/fastapi?charset=utf8mb4"
+    ALLOW_SITE = ["*"]
+    TRUSTED_HOSTS = ["*"]
+#사용자 모드
+@dataclass
+class ProdConfig(Config):
+    ALLOW_SITE = ["*"]
+    TRUSTED_HOSTS = ["*"]
+
+
+def conf():
+    """
+    환경 불러오기
+    :return:
+    """
+    config = dict(prod=ProdConfig(), local=LocalConfig())
+    return config.get(environ.get("API_ENV", "local"))
+
+
---
+++ python/web_framwork/fastapi/app/main.py
@@ -0,0 +1,59 @@
+from os import path as op
+from sys import path as sp
+sp.append(op.dirname(op.dirname(__file__)))
+
+from dataclasses import asdict
+from typing import Optional
+
+import uvicorn
+from fastapi import FastAPI, Depends
+from fastapi.security import APIKeyHeader
+from starlette.middleware.base import BaseHTTPMiddleware
+from starlette.middleware.cors import CORSMiddleware
+
+from app.common.consts import EXCEPT_PATH_LIST, EXCEPT_PATH_REGEX
+from app.database.conn import db
+from app.common.config import conf
+from app.middlewares.token_validator import access_control
+from app.middlewares.trusted_hosts import TrustedHostMiddleware
+from app.routes import index, auth, users, services
+
+
+API_KEY_HEADER = APIKeyHeader(name="Authorization", auto_error=False)
+
+def create_app():
+    """
+    앱 함수 실행
+    :return:
+    """
+    c = conf()
+    app = FastAPI()
+    conf_dict = asdict(c)
+    db.init_app(app, **conf_dict)
+    # 데이터 베이스 이니셜라이즈
+
+    # 레디스 이니셜라이즈
+
+    # 미들웨어 정의
+    app.add_middleware(middleware_class=BaseHTTPMiddleware, dispatch=access_control)
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=conf().ALLOW_SITE,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+    app.add_middleware(TrustedHostMiddleware, allowed_hosts=conf().TRUSTED_HOSTS, except_path=["/health"])
+
+    # 라우터 정의
+    app.include_router(index.router)
+    app.include_router(auth.router, tags=["Authentication"], prefix="/api")
+    app.include_router(services.router, tags=["Services"], prefix="/api")
+    app.include_router(users.router, tags=["Users"], prefix="/api", dependencies=[Depends(API_KEY_HEADER)])
+    return app
+
+
+app = create_app()
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host='127.0.0.1', port=8080,reload=True)
