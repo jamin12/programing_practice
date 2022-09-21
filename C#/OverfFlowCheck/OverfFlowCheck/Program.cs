41168a44566a8212334b01af024681256dc73b38
jamin12 <rudals9901@naver.com>
2022-09-21 11:51:09 +0900

    c# prcatice
    1. checked

---
+++ C#/OverfFlowCheck/OverfFlowCheck/Program.cs
@@ -0,0 +1,15 @@
+﻿int i;
+int max = int.MaxValue;
+try
+{
+    checked
+    {
+        i = max + 1;
+    }
+        Console.WriteLine(max);
+        Console.WriteLine(i);
+}
+catch (Exception e)
+{
+    Console.WriteLine(e.ToString());
+}
--- CS/cshapbasic/cshapbasic/dataType.cs
+++ CS/cshapbasic/cshapbasic/dataType.cs
@@ -1,1 +1,18 @@
-﻿
+﻿double? num1 = null;
+double? num2 = 10.0;
+
+if (num1.HasValue)
+{
+    Console.WriteLine("num1 = " + num1.Value);
+}else
+{
+    Console.WriteLine("num1 dos not have value.");
+}
+if (num2.HasValue)
+{
+    Console.WriteLine("num2 = " + num2.Value);
+}
+else
+{
+    Console.WriteLine("num2 dos not have value.");
+}
