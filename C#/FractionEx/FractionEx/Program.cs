69b03a2cbe9dff5ed0e8adad8fccfbaa0d15eb9e
jamin12 <rudals9901@naver.com>
2022-09-27 14:16:19 +0900

    Feat: c# practice

---
+++ C#/FractionEx/FractionEx/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace myFraction
+{
+    class Fraction
+    {
+        int num1;
+        int num2;
+
+        public Fraction(int num1, int num2)
+        {
+            this.num1 = num1;
+            this.num2 = num2;
+        }
+
+        public void PrintFraction()
+        {
+            Console.WriteLine(num1 + "/" + num2);
+        }
+    }
+
+    class FractionEx
+    {
+        static void Main(String[] args)
+        {
+            Fraction sdf = new Fraction(1, 2);
+            sdf.PrintFraction();
+        }
+    }
+}
