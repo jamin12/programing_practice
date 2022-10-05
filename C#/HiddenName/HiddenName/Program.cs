1f68bd2c4b1b70d254dbe6514637658d26afc82d
jamin12 <rudals9901@naver.com>
2022-10-05 12:22:53 +0900

    c# hidden

---
+++ C#/HiddenName/HiddenName/Program.cs
@@ -0,0 +1,85 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace HiddenName
+{
+    abstract class AAA
+    {
+        protected int a = 1;
+        protected int b = 2;
+
+        public abstract void testA();
+
+        virtual public void MethodA()
+        {
+            Console.WriteLine("AAA-MethodA");
+        }
+        virtual public void MethodB()
+        {
+            Console.WriteLine("BBB-MethodA");
+        }
+        virtual public void MethodC()
+        {
+            Console.WriteLine("CCC-MethodA");
+        }
+    }
+
+    class BBB : AAA
+    {
+        int c = 3;
+        new double b = 4.5;
+        public void OutPut()
+        {
+            Console.WriteLine("{0}\n{1}", a, b);
+        }
+        override public void MethodA()
+        {
+            Console.WriteLine("BBB-AAA-MethodA");
+        }
+        new public void MethodB()
+        {
+            Console.WriteLine("BBB-BBB-MethodB");
+        }
+        override public void MethodC()
+        {
+            Console.WriteLine("BBB-CCC-MethodC");
+        }
+
+        public override void testA()
+        {
+            Console.WriteLine("TEST");
+        }
+    }
+    
+    class CCC : BBB
+    {
+        new public void MethodA()
+        {
+            Console.WriteLine("CCC-AAA-MethodA");
+        }
+        new public void MethodB()
+        {
+            Console.WriteLine("CCC-BBB-MethodB");
+        }
+        override public void MethodC()
+        {
+            Console.WriteLine("CCC-CCC-MethodC");
+        }
+        public new void testA()
+        {
+            Console.WriteLine("CCC-testC");
+        }
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AAA bbb = new BBB();
+            bbb.MethodA();
+            bbb.MethodB();
+            bbb.MethodC();
+            bbb.testA();
+        }
+    }
+}
