3d5c791a71e12f697fb5beebe83d9611369de299
jamin12 <rudals9901@naver.com>
2022-10-12 13:28:58 +0900

    스레드, 제네릭

---
+++ C#/HiddenName/GenericClass/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericClass
+{
+    class Simple<T>
+    {
+        private T[] values;
+        private int index;
+
+        public Simple(int len)
+        {
+            values = new T[len];
+            index = 0;
+        }
+
+        public void add(params T[] args)
+        {
+            foreach (T item in args)
+            {
+                values[index++] = item;
+            }
+        }
+
+        public void print()
+        {
+            foreach (T item in values)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+
+
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Simple<String> s = new Simple<string>(3);
+            Simple<int> s2 = new Simple<int>(3);
+            string[] sArray = { "sdf", "asdf" };
+            s.add(sArray);
+
+            s.print();
+        }
+    }
+}
---
+++ C#/HiddenName/GenericMethod/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericMethod
+{
+    internal class Program
+    {
+        static void Swap<T>(ref T x, ref T y)
+        {
+            (y, x) = (x, y);
+        }
+        static void Main(string[] args)
+        {
+            int a = 1;
+            int b = 2;
+            Swap<int>(ref a, ref b);
+        }
+    }
+}
