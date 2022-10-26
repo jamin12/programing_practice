b813770e8c2488c8d0a974c231f6727507474520
jamin12 <rudals9901@naver.com>
2022-10-26 11:34:05 +0900

    c# 시험 체킹

---
+++ C#/HiddenName/testproejct/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testproejct
+{
+    class ttes
+    {
+        public int i = 0;
+    }
+    struct teststruct
+    {
+        public int a;
+        public teststruct(int a)
+        {
+            this.a = a;
+        }
+    }
+    internal class Program
+    {
+        static void ao(object obj)
+        {
+            Console.WriteLine(obj + " " + (obj as string));
+            var ii = obj as string;
+            Console.WriteLine(ii.GetType());
+        }
+        static void aao(ttes s)
+        {
+            s.i = 5;
+        }
+
+        static void aa11(teststruct teststruct)
+        {
+            teststruct.a = 11;
+        }
+
+        
+        static void Main(string[] args)
+        {
+            ttes ao = new ttes();
+            aao(ao);
+            Console.WriteLine(ao.i);
+            teststruct aaa = new teststruct(1);
+            aa11(aaa);
+            Console.WriteLine(aaa.a);
+        }
+    }
+}
---
+++ C#/test/test/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    class myClass<T>
+    {
+        private T[] values;
+        private int index;
+        public myClass(int len)
+        {
+            values = new T[len];
+            index = 0;
+        }
+
+        public int Index
+        {
+            get { return index; }
+            set { index = value; }
+        }
+
+        public void add(params T[] args)
+        {
+            foreach (T arg in args)
+            {
+
+            }
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
