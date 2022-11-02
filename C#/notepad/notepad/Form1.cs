c52897e232e069cc3ae1b1c97d6433befe6fe01d
jamin12 <rudals9901@naver.com>
2022-11-02 12:21:50 +0900

    notepad making

---
+++ C#/notepad/notepad/Form1.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace notepad
+{
+    public partial class Form1 : Form
+    {
+        private Boolean txtNoteChange; // 내용 변경 확인
+        private String fWord; // 찾기 문자열
+        private Form2 frm2; // 찾기 폼
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void 열기OToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (txtNoteChange)
+            {
+                var msg = Text + " 파일 내용이 변경되었습니다.\r\n변경된 내용을 저장하시겠습니까?";
+                DialogResult dir = MessageBox.Show(msg, "메모장", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Question);
+
+                if (dir == DialogResult.Yes)
+                {
+                    txtSave();
+                    txtOpen();
+                }
+            }
+            else
+            {
+                txtOpen();
+            }
+        }
+
+        private void txtOpen()
+        {
+            DialogResult dir = openFileDialog1.ShowDialog();
+            if (dir == DialogResult.OK)
+            {
+                String str = openFileDialog1.FileName;
+                StreamReader sr = new StreamReader(str, System.Text.Encoding.Default);
+                txtNote.Text = sr.ReadToEnd();
+                sr.Close();
+
+                FileInfo f = new FileInfo(str);
+                Text = f.Name;
+                txtNoteChange = false;
+            }
+        }
+
+        private void 저장SToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            txtSave();
+        }
+
+        private void txtSave()
+        {
+            if (Text == "메모장")
+            {
+                DialogResult dialogResult = saveFileDialog1.ShowDialog();
+                if (dialogResult != DialogResult.Cancel)
+                {
+                    String str = saveFileDialog1.FileName;
+                    StreamWriter sw = new StreamWriter(str, false, System.Text.Encoding.Default);
+                    sw.Write(txtNote.Text);
+                    sw.Flush();
+                    sw.Close();
+
+                    FileInfo fileInfo = new FileInfo(str);
+                    Text = fileInfo.Name;
+                }
+            }
+            else
+            {
+                String str = Text;
+                StreamWriter sw = new StreamWriter(str, false, System.Text.Encoding.Default);
+                sw.Write(txtNote.Text);
+                sw.Flush();
+                sw.Close();
+            }
+        }
+
+        private void txtNote_TextChanged(object sender, EventArgs e)
+        {
+            txtNoteChange = true;
+        }
+
+        private void 새로만들기NToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (txtNoteChange)
+            {
+                var msg = Text + " 파일 내용이 변경되었습니다.\r\n변경된 내용을 저장하시겠습니까?";
+                DialogResult dir = MessageBox.Show(msg, "메모장", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Question);
+
+                if (dir == DialogResult.Yes)
+                {
+                    txtSave();
+                    txtNote.ResetText();
+                    Text = "메모장";
+                }
+                else if (dir == DialogResult.No)
+                {
+                    txtNote.ResetText();
+                    Text = "메모장";
+                    txtNoteChange = false;
+                }
+                else
+                {
+                    return;
+                }
+            }
+            else
+            {
+                txtNote.ResetText();
+                Text = "메모장";
+                txtNoteChange = false;
+            }
+        }
+
+        private void 다른이름으로저장AToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DialogResult dir = saveFileDialog1.ShowDialog();
+            if(dir == DialogResult.Yes)
+            {
+                String str = saveFileDialog1.FileName;
+                StreamWriter sw = new StreamWriter(str, false, System.Text.Encoding.Default);
+                sw.Write(txtNote.Text);
+                sw.Flush();
+                sw.Close();
+
+                FileInfo fileInfo = new FileInfo(str);
+                Text = fileInfo.Name;
+                txtNoteChange = false;
+            }
+        }
+
+        private void 끝내기QToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (txtNoteChange)
+            {
+                var msg = Text + " 파일 내용이 변경되었습니다.\r\n변경된 내용을 저장하시겠습니까?";
+                DialogResult dir = MessageBox.Show(msg, "메모장", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Question);
+                if(dir == DialogResult.Yes)
+                {
+                    txtSave();
+                    Close();
+                }
+                else
+                {
+                    return;
+                }
+            }
+            Close();
+        }
+
+        private void 자동줄바꿈WToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            자동줄바꿈WToolStripMenuItem.Checked = !(자동줄바꿈WToolStripMenuItem.Checked);
+            txtNote.WordWrap = !(txtNote.WordWrap);
+        }
+
+        private void 글꼴ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DialogResult dir = fontDialog1.ShowDialog();
+            if(dir == DialogResult.OK)
+            {
+                txtNote.Font = fontDialog1.Font;
+            }
+        }
+    }
+}
---
+++ C#/notepad/notepad/Form2.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace notepad
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtWord = new System.Windows.Forms.TextBox();
+            this.btnOk = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.chkCase = new System.Windows.Forms.CheckBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.s = new System.Windows.Forms.RadioButton();
+            this.rdBtnDown = new System.Windows.Forms.RadioButton();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "찾을 내용";
+            // 
+            // txtWord
+            // 
+            this.txtWord.Location = new System.Drawing.Point(107, 33);
+            this.txtWord.Name = "txtWord";
+            this.txtWord.Size = new System.Drawing.Size(366, 25);
+            this.txtWord.TabIndex = 1;
+            // 
+            // btnOk
+            // 
+            this.btnOk.Location = new System.Drawing.Point(485, 36);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(84, 23);
+            this.btnOk.TabIndex = 2;
+            this.btnOk.Text = "다음 찾기";
+            this.btnOk.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnOk.UseVisualStyleBackColor = true;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(485, 86);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(84, 23);
+            this.btnCancel.TabIndex = 3;
+            this.btnCancel.Text = "취소";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // chkCase
+            // 
+            this.chkCase.AutoSize = true;
+            this.chkCase.Location = new System.Drawing.Point(32, 99);
+            this.chkCase.Name = "chkCase";
+            this.chkCase.Size = new System.Drawing.Size(130, 19);
+            this.chkCase.TabIndex = 4;
+            this.chkCase.Text = "대/소문자 구분";
+            this.chkCase.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.s);
+            this.groupBox1.Controls.Add(this.rdBtnDown);
+            this.groupBox1.Location = new System.Drawing.Point(183, 74);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(247, 55);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "방향";
+            // 
+            // s
+            // 
+            this.s.AutoSize = true;
+            this.s.Location = new System.Drawing.Point(38, 24);
+            this.s.Name = "s";
+            this.s.Size = new System.Drawing.Size(58, 19);
+            this.s.TabIndex = 6;
+            this.s.TabStop = true;
+            this.s.Text = "위쪽";
+            this.s.UseVisualStyleBackColor = true;
+            // 
+            // rdBtnDown
+            // 
+            this.rdBtnDown.AutoSize = true;
+            this.rdBtnDown.Checked = true;
+            this.rdBtnDown.Location = new System.Drawing.Point(134, 24);
+            this.rdBtnDown.Name = "rdBtnDown";
+            this.rdBtnDown.Size = new System.Drawing.Size(73, 19);
+            this.rdBtnDown.TabIndex = 7;
+            this.rdBtnDown.TabStop = true;
+            this.rdBtnDown.Text = "아래쪽";
+            this.rdBtnDown.UseVisualStyleBackColor = true;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(606, 172);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.chkCase);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnOk);
+            this.Controls.Add(this.txtWord);
+            this.Controls.Add(this.label1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtWord;
+        private System.Windows.Forms.Button btnOk;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.CheckBox chkCase;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rdBtnDown;
+        private System.Windows.Forms.RadioButton s;
+    }
+}
