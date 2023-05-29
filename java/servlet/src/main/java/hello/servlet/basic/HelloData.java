333c1dae20dac3729cbefbaf0adce2f4eb633387
jamin12 <rudals9901@naver.com>
2023-05-29 18:14:40 +0900

    feat: http request 받기

---
+++ java/servlet/src/main/java/hello/servlet/basic/HelloData.java
@@ -0,0 +1,15 @@
+package hello.servlet.basic;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class HelloData {
+    private String username;
+    private int age;
+}
