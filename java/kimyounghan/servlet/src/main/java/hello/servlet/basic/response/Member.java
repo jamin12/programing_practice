fb86e296b7769343053333e7a678eebe0c359e4f
jamin12 <rudals9901@naver.com>
2024-01-20 12:22:10 +0900

    feat: servlet jsp mvc 순서대로 구현

---
+++ java/kimyounghan/servlet/src/main/java/hello/servlet/basic/response/Member.java
@@ -0,0 +1,19 @@
+package hello.servlet.basic.response;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class Member {
+    private Long id;
+    private String userName;
+    private int age;
+
+    public Member( String userName, int age) {
+        this.userName = userName;
+        this.age = age;
+    }
+}
