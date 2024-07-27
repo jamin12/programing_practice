505e2331a20e983e3124bc1774159ffd43650798
rudal <rudals9901@naver.com>
2024-07-27 13:27:16 +0900

    feat: jpa

--- java/kimyounghan/db2/src/main/java/hello/itemservice/domain/Item.java
+++ java/kimyounghan/db2/src/main/java/hello/itemservice/domain/Item.java
@@ -1,22 +1,29 @@
 package hello.itemservice.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 import lombok.Data;
 
 @Data
+@Entity
 public class Item {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
-    private Long id;
+	private String itemName;
+	private Integer price;
+	private Integer quantity;
 
-    private String itemName;
-    private Integer price;
-    private Integer quantity;
+	public Item() {
+	}
 
-    public Item() {
-    }
-
-    public Item(String itemName, Integer price, Integer quantity) {
-        this.itemName = itemName;
-        this.price = price;
-        this.quantity = quantity;
-    }
+	public Item(String itemName, Integer price, Integer quantity) {
+		this.itemName = itemName;
+		this.price = price;
+		this.quantity = quantity;
+	}
 }
