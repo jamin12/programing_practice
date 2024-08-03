2528d992481c422b02526330539206276360f890
rudal <rudals9901@naver.com>
2024-08-03 16:45:10 +0900

    feat: 실용적인 구조

--- java/kimyounghan/db2/src/main/java/hello/itemservice/repository/v2/ItemQueryRepositoryV2.java
+++ java/kimyounghan/db2/src/main/java/hello/itemservice/repository/v2/ItemQueryRepositoryV2.java
@@ -1,56 +1,29 @@
-package hello.itemservice.repository.jpa;
+package hello.itemservice.repository.v2;
 
 import static hello.itemservice.domain.QItem.*;
 
 import java.util.List;
-import java.util.Optional;
 
 import javax.persistence.EntityManager;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StringUtils;
 
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 
 import hello.itemservice.domain.Item;
-import hello.itemservice.repository.ItemRepository;
 import hello.itemservice.repository.ItemSearchCond;
-import hello.itemservice.repository.ItemUpdateDto;
 
 @Repository
-@Transactional
-public class JpaItemRepositoryV3 implements ItemRepository {
-	private final EntityManager em;
+public class ItemQueryRepositoryV2 {
 	private final JPAQueryFactory jpaQueryFactory;
 
-	public JpaItemRepositoryV3(EntityManager em) {
-		this.em = em;
+	public ItemQueryRepositoryV2(EntityManager em) {
 		this.jpaQueryFactory = new JPAQueryFactory(em);
 	}
 
-	@Override
-	public Item save(Item item) {
-		em.persist(item);
-		return item;
-	}
-
-	@Override
-	public void update(Long itemId, ItemUpdateDto updateParam) {
-		Item item = em.find(Item.class, itemId);
-		item.setItemName(updateParam.getItemName());
-		item.setPrice(updateParam.getPrice());
-		item.setQuantity(updateParam.getQuantity());
-	}
-
-	@Override
-	public Optional<Item> findById(Long id) {
-		Item item = em.find(Item.class, id);
-		return Optional.ofNullable(item);
-	}
-
-	@Override
 	public List<Item> findAll(ItemSearchCond cond) {
 		return jpaQueryFactory.select(item)
 			.from(item)
@@ -66,3 +39,4 @@ public class JpaItemRepositoryV3 implements ItemRepository {
 		return maxPrice == null ? null : item.price.loe(maxPrice);
 	}
 }
+
